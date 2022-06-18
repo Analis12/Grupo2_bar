d2b1c0c054aecc077f5ddae953d4259c40078fdf
Analis12 <69524622+Analis12@users.noreply.github.com>
2022-06-18 13:17:16 -0500

    CSS

--- vistas/paginaprincipal.php
+++ vistas/paginaprincipal.php
@@ -4,11 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Bar</title>
+    <link rel="stylesheet" href="../css/estilos.css">
 </head>
 <body>
-    <p>Pagina principal</p>
-    <a href='vistas/consulta1.php'>Consulta 1</a>
-    <a href='vistas/consultas2.php'>Consulta 2</a>
+<div class="contenedor">
+        <img  src="../imagenes/principal2.jpg" />
+        <div class="centrado">
+        <p><h1> El Cafesito</h1> 
+                <h3> Ofrece: </h3></p>
+            <button class="consulta1" onclick="location.href='vistas/consulta1.php'" >Consulta 1 </button>
+           <button  class="consulta2" onclick="location.href='vistas/consultas2.php'" >Consulta 2 </button>
+        </div>
+      </div>
 </body>
 </html>
